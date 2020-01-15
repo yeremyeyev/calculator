bfcd4ff5b291b76fdd4f145edb47bacf27173d53
Eremeev Andery <a.yeremyeyev@gmail.com>
2020-01-15 22:05:20 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// document.getElementById("btnAllNumbrs").addEventListener("click", function (){
+//     let div = document.createElement("div");
+//     div.innerHTML = btnAllNumbrs.value;
+//     document.body.append(div);
+//     console.log(btnAllNumbrs.value);
+// });
+
+// это работет но совсем не правильно___________________________
+// const presseBtn = document.getElementById("btnAllNumbrs");
+// addEventListener("click", function (){
+//     let div = document.createElement("div");
+//     div.innerHTML = presseBtn.value;
+//     document.getElementById('disp').append(presseBtn);
+//     console.log(presseBtn.value);
+// });
+// __________________________
+const presseBtn = document.querySelectorAll('.numbers');
+
+
+
+
