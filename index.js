79d6a471ec953a7ac41b3fa10000f4a1761d64c6
Eremeev Andrey <a.yeremyeyev@gmail.com>
2020-01-16 16:50:38 +0200

    2
    2

--- index.js
+++ index.js
@@ -5,17 +5,23 @@
 //     console.log(btnAllNumbrs.value);
 // });
 
-это работет но совсем не правильно___________________________
-const presseBtn = document.getElementById("btnAllNumbrs");
-addEventListener("click", function (){
-    let div = document.createElement("div");
-    div.innerHTML = presseBtn.value;
-    document.getElementById('disp').append(presseBtn);
-    console.log(presseBtn.value);
-});
-__________________________
-const presseBtn = document.querySelectorAll('.numbers');
+// это работет но совсем не правильно___________________________
+// const presseBtn = document.getElementById("btnAllNumbrs");
+// addEventListener("click", function (){
+//     let div = document.createElement("div");
+//     div.innerHTML = presseBtn.value;
+//     document.getElementById('disp').append(presseBtn);
+//     console.log(presseBtn.value);
+// });
+// __________________________
+// const presseBtn = document.querySelectorAll(".numbers");
+// function click() {
+//   let div = document.createElement("div");
+//   div.innerHTML = presseBtn.value;
+//   document.getElementById("disp").append(presseBtn);
+//   console.log(presseBtn.value);
+// }
+let first_number;
+let second_number;
 
-
-
-
+onclick="calc.input.value += '1'"
